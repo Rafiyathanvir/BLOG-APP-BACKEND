76c1e06eda3aa75ef050c8095338fdd7674be3d7
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-17 19:52:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'https://moonlit-fairy-34c09e.netlify.app'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
