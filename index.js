f7dcce104520e594eb23463b769a890ea3a5904d
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-02-18 02:20:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 //app.use(cors());
-app.use(cors({origin:'*'}));
+app.use(cors());
 
 
 app.use(express.json());
