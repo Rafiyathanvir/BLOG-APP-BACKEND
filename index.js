ebccf2bbe6893b256996a33a7818fa1be8af7a60
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-03-12 23:36:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 //app.use(cors());
 //app.use(cors());
-app.use(cors({credentials:true,origin:'https://blog-app-backend-nu.vercel.app'}));
+app.use(cors({credentials:true,origin:'*'}));
 
 
 
