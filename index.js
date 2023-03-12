0341524c84f72567171281f37fcb5ae3bf0b1aad
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2023-03-12 23:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 //app.use(cors());
 //app.use(cors());
-app.use(cors({credentials:true,origin:'https://blog-app-up.onrender.com'}));
+app.use(cors({credentials:true,origin:'*'}));
 
 
 
